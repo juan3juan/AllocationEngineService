7826da45add86de91a90d97e9d61d6989192e260
jihanzhang <dreck.zhang@gmail.com>
2018-07-25 15:40:49 -0400

    first AllocationEngineService

---
+++ AllocationEngineService/Controllers/AllocationEngineController.cs
@@ -0,0 +1,58 @@
+﻿using AllocationEngineService.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AllocationEngineService.Controllers
+{
+    [Route("api/[Controller]")]
+    public class AllocationEngineController : Controller
+    {
+        //[HttpPost("[action]")]
+        //public async Task<IActionResult> Strategy([FromBody] DataContract dataContract)
+        //{
+        //    double Cash = dataContract.CurrentCash;
+        //    return Ok("Cash remail :" + Cash / 3);
+        //}
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Strategy([FromBody] DataContract dataContract)
+        {
+            //DataContract dataContract = JsonConvert.DeserializeObject<DataContract>(dataContractstring);
+            Dictionary<string, int> result = new Dictionary<string, int>();
+
+            SecPosition secPosition = dataContract.SecPositions.First();
+            string SecurityID = secPosition.SecurityID;
+            double cash = dataContract.CurrentCash;
+            List<double> priceHistory = secPosition.CurrentPrice;
+            double currentPrice = priceHistory.Last();
+            double previousPrice = priceHistory[priceHistory.Count - 2];
+            bool flagBuy = secPosition.PositionQuantity > 0 ? true : false;
+
+            #region logic to buy or sell stock
+            if ((previousPrice > currentPrice * 1.03) && flagBuy == false)
+            {
+                #region Buy
+                flagBuy = true;
+                int quantity = (int)(cash / currentPrice);
+                result.Add(SecurityID, quantity);
+                #endregion Buy
+            }
+            else if (flagBuy == true)
+            {
+                #region Sell
+                if (currentPrice > secPosition.BuyPrice * 1.05)
+                {
+                    result.Add(SecurityID, -1 * secPosition.PositionQuantity);
+                }
+                #endregion Sell
+            }
+            #endregion logic to buy or sell stock
+            //string x = JsonConvert.SerializeObject(result);
+            return Ok(result);
+        }
+    }
+}
---
+++ AllocationEngineService/Model/DataContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllocationEngineService.Model
+{
+    public class DataContract
+    {
+        public double CurrentCash;
+        public List<SecPosition> SecPositions;
+
+        public DataContract()
+        {
+            SecPositions = new List<SecPosition>();
+        }
+    }
+}
